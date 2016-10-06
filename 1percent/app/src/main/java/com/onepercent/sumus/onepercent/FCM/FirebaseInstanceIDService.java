8d890048392f87cccd7bd93d76ee8a4bfc41dce9
sunahs <tjsdkz3@naver.com>
2016-10-07 03:41:26 +0900

    FCM 완료

---
+++ 1percent/app/src/main/java/com/onepercent/sumus/onepercent/FCM/FirebaseInstanceIDService.java
@@ -0,0 +1,27 @@
+package com.onepercent.sumus.onepercent.FCM;
+
+/**
+ * Created by MINI on 2016-10-07.
+ */
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/*
+사용자 기기별 token을 생성하는 클래스 입니다.
+나중에 push 알림을 특정 타겟에게 보낼 때 사용되는 고유 키 값 입니다.
+이 토큰값을 용도에 맞게 사용하시면 됩니다
+*/
+public class FirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "MyFirebaseIIDService";
+
+    // [START refresh_token]
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String token = FirebaseInstanceId.getInstance().getToken();
+
+    }
+
+}
--- 1percent/app/src/main/java/com/onepercent/sumus/onepercent/LoginActivity.java
+++ 1percent/app/src/main/java/com/onepercent/sumus/onepercent/LoginActivity.java
@@ -3,19 +3,31 @@ package com.onepercent.sumus.onepercent;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
     ImageButton login_loginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
         startActivity(new Intent(this, SplashActivity.class)); // 스플래쉬
         login_loginBtn = (ImageButton)findViewById(R.id.login_loginBtn);
         login_loginBtn.setOnClickListener(this);
+
+
+        // FCM
+        String Token = FirebaseInstanceId.getInstance().getToken();
+        Log.d("SUN",Token);
+        FirebaseMessaging.getInstance().subscribeToTopic("notice");
     }
 
     @Override
